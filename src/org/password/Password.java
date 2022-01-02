165a09559ebcdfc304ceaa0a55e1952c0b2972e7
FaceBook <mail2afroze@gmail.com>
2022-01-02 10:33:02 +0530

    Password Function

--- src/org/password/Password.java
+++ src/org/password/Password.java
@@ -1,5 +1,12 @@
 package org.password;
 
 public class Password {
+	public static void password1() {
+		System.out.println("Password function completed");
+	}
+	
+	public static void main(String[] args) {
+		password1();
+	}
 
 }
